e8b2d8daf616dae193afb398f9cf24cec8a12959
lorussogiacomoluca <lorussogiacomoluca@gmail.com>
2025-03-18 16:42:39 +0100

    [UPD] optimize code to display FizzBuzz with comments

--- js/script.js
+++ js/script.js
@@ -2,16 +2,23 @@
 //Individuare i numeri multipli di 3
 //Individuare i numeri multipli di 5
 
+
 for(let i=1;i<=100;i++){
+    //Verifico se il numero e' divisibile per tre
 if(i%3 == 0){
+    //Verifico se e' contemporaneamente divisibile per 5 ->FizzBuzz
     if(i%5==0){
         console.log(i,' FizzBuzz');
+    //Se non e' divisibile anche per 5, allora ->Fizz
     }else{
         console.log(i,' Fizz');
     }
+//Verifico se il numero e' divisibile per 5 -> Buzz
 }else if(i%5 == 0){
     console.log(i,' Buzz');
+//Identifico i numeri non divisibile per 3 AND 5
 }else{
     console.log(i);
 }
 }
+
