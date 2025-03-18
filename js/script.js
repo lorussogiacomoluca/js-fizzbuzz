d83a5d1be8c101c2ebd173d1e731be1bf2feeee1
lorussogiacomoluca <lorussogiacomoluca@gmail.com>
2025-03-18 16:37:50 +0100

    [UPD] display fizzbuzz

--- js/script.js
+++ js/script.js
@@ -5,12 +5,12 @@
 for(let i=1;i<=100;i++){
 if(i%3 == 0){
     if(i%5==0){
-        console.log('FizzBuzz');
+        console.log(i,' FizzBuzz');
     }else{
-        console.log('Fizz');
+        console.log(i,' Fizz');
     }
 }else if(i%5 == 0){
-    console.log('Buzz');
+    console.log(i,' Buzz');
 }else{
     console.log(i);
 }
